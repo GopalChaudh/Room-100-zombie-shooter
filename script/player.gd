9de7cc60bf8af7761a503d8a411e5de45dea1749
GopalChaudh <kc7198931@gmail.com>
2024-06-06 11:40:20 +0530

    basic scene setup and added enemies and player

---
+++ script/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+
+@export var SPEED:float = 300.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
